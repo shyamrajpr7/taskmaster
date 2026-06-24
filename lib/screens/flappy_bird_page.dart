d045a6a536eade96d6e8137b87c5678baf93bb92
game <shyamraj.p.r777@gmail.com>
2026-06-24 12:10:43 +0530

    fix: add missing Ticker import

--- lib/screens/flappy_bird_page.dart
+++ lib/screens/flappy_bird_page.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import '../services/gamification_service.dart';
 
 const Color _bgCanvas = Color(0xFF0F172A);
